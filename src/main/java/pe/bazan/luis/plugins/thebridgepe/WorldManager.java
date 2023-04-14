7d36963c00ff3b1d4fa226f50edd189939febd70
Luis Bazan <luis@bazan.pe>
2023-04-14 15:37:28 -0500

    Create a getFreeWorld method

--- src/main/java/pe/bazan/luis/plugins/thebridgepe/WorldManager.java
+++ src/main/java/pe/bazan/luis/plugins/thebridgepe/WorldManager.java
@@ -1,8 +1,11 @@
 package pe.bazan.luis.plugins.thebridgepe;
 
+import com.onarandombox.MultiverseCore.MVWorld;
 import com.onarandombox.MultiverseCore.MultiverseCore;
 import com.onarandombox.MultiverseCore.api.MultiverseWorld;
 import org.bukkit.Bukkit;
+import org.bukkit.World;
+import org.bukkit.WorldType;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,6 +13,7 @@ import java.util.List;
 public class WorldManager {
     private MultiverseCore multiverseCore;
     private List<MultiverseWorld> worlds;
+    private List<MultiverseWorld> freeWorlds;
 
     public WorldManager() {
         this.multiverseCore = (MultiverseCore) Bukkit.getServer().getPluginManager().getPlugin("Multiverse-Core");
@@ -18,6 +22,12 @@ public class WorldManager {
     }
 
     private void registerWorlds() {
+        World world = Bukkit.getWorld("the-bridge");
+
+        if (world == null) {
+            multiverseCore.getMVWorldManager().addWorld("the-bridge", World.Environment.NORMAL, "seed", WorldType.FLAT, false, "TheBridgePe");
+        }
+
         for (MultiverseWorld multiverseWorld : multiverseCore.getMVWorldManager().getMVWorlds()) {
             if (multiverseWorld.getName().startsWith("the-bridge")) worlds.add(multiverseWorld);
         }
@@ -27,8 +37,9 @@ public class WorldManager {
         for (MultiverseWorld multiverseWorld : worlds) {
             String name = multiverseWorld.getName();
             multiverseCore.deleteWorld(name);
-            multiverseCore.cloneWorld("world", name, "");
+            multiverseCore.cloneWorld("the-bridge", name, "TheBridgePe");
         }
+        freeWorlds = worlds;
     }
 
     public MultiverseCore getMultiverseCore() {
@@ -37,5 +48,21 @@ public class WorldManager {
 
     public List<MultiverseWorld> getWorlds() {
         return worlds;
+    }
+
+    public MultiverseWorld getFreeWorld() {
+        if (freeWorlds.size() >= 1) return freeWorlds.remove(0);
+        else return generateWorld();
+    }
+
+    public MultiverseWorld generateWorld() {
+        String worldName = String.format("the-bridge-%s", worlds.size());
+        multiverseCore.cloneWorld("the-bridge", worldName, "TheBridgePe");
+        MultiverseWorld world = multiverseCore.getMVWorldManager().getMVWorld(worldName);
+        if (world != null) {
+            worlds.add(world);
+            return world;
+        }
+        return null;
     }
 }
