a422c4eb9f2a8c72e8902aa26f2180f8dbeec532
Luis Bazan <luis@bazan.pe>
2023-04-19 08:39:14 -0500

    Add block breake event

--- src/main/java/pe/bazan/luis/plugins/thebridgepe/events/arena/BlocksEvents.java
+++ src/main/java/pe/bazan/luis/plugins/thebridgepe/events/arena/BlocksEvents.java
@@ -1,7 +1,9 @@
 package pe.bazan.luis.plugins.thebridgepe.events.arena;
 
+import com.sk89q.worldedit.event.platform.BlockInteractEvent;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import pe.bazan.luis.plugins.thebridgepe.GameManager;
 import pe.bazan.luis.plugins.thebridgepe.TheBridgePe;
@@ -17,5 +19,21 @@ public class BlocksEvents implements Listener {
 
         if (!gameManager.getArenaFromPlayer(player).inBuildBox(e.getBlock().getLocation()))
             e.setCancelled(true);
+    }
+
+    public void onPlayerBreakeBlock(BlockBreakEvent e) {
+        Player player = e.getPlayer();
+        GameManager gameManager = TheBridgePe.getInstance().getCurrentGame();
+        if (
+                gameManager == null
+                        || gameManager.getArenaFromPlayer(player) == null
+        ) return;
+
+        if (!gameManager.getArenaFromPlayer(player).inBuildBox(e.getBlock().getLocation()))
+            e.setCancelled(true);
+    }
+
+    public void onPlayerInteractWithBLock(BlockInteractEvent e) {
+
     }
 }
