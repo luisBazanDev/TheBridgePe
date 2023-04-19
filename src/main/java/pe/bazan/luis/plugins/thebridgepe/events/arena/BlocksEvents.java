49bc45daf27f9868493aaa9e60e567bc6e9890b0
Luis Bazan <luis@bazan.pe>
2023-04-19 08:35:10 -0500

    Create a BlocksEvents in arenas

---
+++ src/main/java/pe/bazan/luis/plugins/thebridgepe/events/arena/BlocksEvents.java
@@ -0,0 +1,21 @@
+package pe.bazan.luis.plugins.thebridgepe.events.arena;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockPlaceEvent;
+import pe.bazan.luis.plugins.thebridgepe.GameManager;
+import pe.bazan.luis.plugins.thebridgepe.TheBridgePe;
+
+public class BlocksEvents implements Listener {
+    public void onPLayerPlaceBlock(BlockPlaceEvent e) {
+        Player player = e.getPlayer();
+        GameManager gameManager = TheBridgePe.getInstance().getCurrentGame();
+        if (
+                gameManager == null
+            || gameManager.getArenaFromPlayer(player) == null
+        ) return;
+
+        if (!gameManager.getArenaFromPlayer(player).inBuildBox(e.getBlock().getLocation()))
+            e.setCancelled(true);
+    }
+}
